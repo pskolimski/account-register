d4f4e80510075ac7b2c2c4cf231c54371e52faa6
Patryk Skolimowski <zsmi@revku.dev>
2024-01-03 13:23:14 +0100

    Update

--- app/src/main/java/com/example/rejestrujkonto/MainActivity.kt
+++ app/src/main/java/com/example/rejestrujkonto/MainActivity.kt
@@ -2,13 +2,30 @@ package com.example.rejestrujkonto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.rejestrujkonto.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val button: Button = binding.registerButton
+        val emailInput = binding.emailInput
+        val passwordInput = binding.passwordInput
+        val repeatPasswordInput = binding.repeatPasswordInput
+        val feedback = binding.feedback
+
+        button.setOnClickListener() {
+            if (!emailInput.text.toString().contains("@")) {
+                feedback.text = "Nieprawidłowy adres e-mail"
+            } else if (passwordInput.text.toString() != repeatPasswordInput.text.toString()) {
+                feedback.text = "Hasła się różnią"
+            } else {
+                feedback.text = "Witaj " + emailInput.text.toString()
+            }
+        }
     }
 }
